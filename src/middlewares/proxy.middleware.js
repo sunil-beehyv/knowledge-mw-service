c295b6ddf124d61b64bd4f1396733b9f70e464ce
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-02-19 14:41:28 +0530

    Issue #000 fix: content url proxy.

--- src/middlewares/proxy.middleware.js
+++ src/middlewares/proxy.middleware.js
@@ -293,7 +293,7 @@ module.exports = function (app) {
   )
   app.use(
     ['/action/framework/v3/read/*', '/action/content/v3/read/*', '/action/content/v1/read/*',
-      '/action/content/v3/hierarchy/*'],
+      '/action/content/v3/hierarchy/*', '/action/content/v3/url/*'],
     proxy(contentRepoBaseUrl, {
       limit: reqDataLimitOfContentUpload,
       proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
